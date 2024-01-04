7a8aef527600d8ef4900a844ddbb5bed650788dc
CHVYArepaxa <129049242+CHVYArepaxa@users.noreply.github.com>
2024-01-04 17:34:49 +0300

    Update permissions.py

--- backend/foodgram/recipes/permissions.py
+++ backend/foodgram/recipes/permissions.py
@@ -5,7 +5,7 @@ from .models import Recipe
 
 
 class IsAuthorOrReadOnly(BasePermission):
-    def has_object_permission(self, request: Request, view, obj: Recipe):
+    def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         return obj.author == request.user
